d3e91330eab9bdab7b73d0f2dbbfc9fc6018c234
Daniel Peña Iglesias <dpenai@indra.es>
2018-09-19 10:37:00 +0200

    [MOD] Added external tracing writing and config for localstorage

--- projects/ngrx-system/src/lib/storeConfig/system.metareducers.ts
+++ projects/ngrx-system/src/lib/storeConfig/system.metareducers.ts
@@ -3,9 +3,9 @@ import { localStorageMeta } from './metareducers/localStorage.metareducer';
 import { loggerMeta } from './metareducers/logger.metareducer';
 import { hardResetMeta } from './metareducers/hardReset.metareducer';
 
-export const metaReducers = config => [
-  loggerMeta(config),
+export const metaReducers = [
+  loggerMeta,
   hardResetMeta,
-  errorTracingMeta(config),
-  localStorageMeta,
+  errorTracingMeta,
+  localStorageMeta
 ];
